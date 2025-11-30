6e62bc477e23de323fc72ddd96d442f0ad2ff49e
ma7aseno2002 <ma7aseno2002@gmail.com>
2025-11-30 20:49:01 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vite'
 
 export default defineConfig({
-  plugins: [react()],
   build: { outDir: 'dist' }
 })
